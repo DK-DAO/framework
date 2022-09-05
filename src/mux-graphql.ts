037397cf1a1bc4342f680c233ba53e10f99fa78c
kevin <dqtkien@gmail.com>
2022-09-05 09:46:09 +0700

    Add GraphQL resolver in Mux

--- src/mux-graphql.ts
+++ src/mux-graphql.ts
@@ -3,20 +3,21 @@ import Joi from 'joi';
 export class MuxGraphQl {
   private static production: boolean = true;
 
-  public static init(production: boolean = true) {
+  public static initGraphQl(production: boolean = true) {
     MuxGraphQl.production = production;
   }
 
-  public static handler(schema: Joi.ObjectSchema, resolver: (...params: any[]) => Promise<any>) {
+  public static resolver(schema: Joi.ObjectSchema, handler: (...params: any[]) => Promise<any>) {
     return (root: any, args: any, context: any, info: any) => {
       const validator = schema.validate(args);
       if (validator.error) {
+        const errorString = `${validator.error.name}: ${validator.error.message}`;
         if (MuxGraphQl.production) {
-          throw new Error('Invalid user input');
+          throw new Error(errorString);
         }
-        throw new Error(validator.error.message);
+        throw new Error(`${errorString} in ${validator.error.stack}`);
       }
-      return resolver(root, validator.value, context, info);
+      return handler(root, validator.value, context, info);
     };
   }
 }
